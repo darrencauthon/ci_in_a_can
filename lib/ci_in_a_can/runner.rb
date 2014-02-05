8c4fb411b5ebfb4efd40762623e95ce2c4088ef8
Darren Cauthon <darren@cauthon.com>
2014-02-04 22:04:05 -0600

    Clone the repository every time a new json file is dropped.

--- lib/ci_in_a_can/runner.rb
+++ lib/ci_in_a_can/runner.rb
@@ -3,7 +3,8 @@ module CiInACan
   module Runner
 
     def self.run data
-      puts data.inspect
+      `git clone git@github.com:darrencauthon/ci_in_a_can.git temp/#{data['unique_location']}`
+      puts `cd temp/#{data['unique_location']}; ls`
     end
 
   end
--- lib/ci_in_a_can/watcher.rb
+++ lib/ci_in_a_can/watcher.rb
@@ -6,10 +6,11 @@ module CiInACan
   module Watcher
 
     def self.watch
-      listener = ::Listen.to('.') do |modified, added, removed|
+      listener = ::Listen.to('.', only: /\.json$/) do |modified, added, removed|
         return unless added.count > 0
         json = File.read added.first
-        data = JSON.parse(json)['payload']
+        data = JSON.parse(JSON.parse(json)['payload'])
+        data['unique_location'] = UUID.new.generate
         Runner.run data
       end
       listener.start
