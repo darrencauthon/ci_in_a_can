41011fc080f5ac1d5fc8166c648aa6872b37e330
Darren Cauthon <darren@cauthon.com>
2014-02-07 15:15:06 -0600

    Send failing results to Github as well.

--- spec/ci_in_a_can/test_result_notifier_spec.rb
+++ spec/ci_in_a_can/test_result_notifier_spec.rb
@@ -22,6 +22,21 @@ describe CiInACan::TestResultNotifier do
       CiInACan::TestResultNotifier.send_for build, test_result
     end
 
+    it "should mark failing tests as failure in github" do
+
+      test_result.passed = false
+
+      build.sha  = Object.new
+      build.repo = Object.new
+
+      client = Object.new
+      CiInACan::Github.stubs(:client).returns client
+
+      client.expects(:create_status).with(build.repo, build.sha, "failure")
+
+      CiInACan::TestResultNotifier.send_for build, test_result
+    end
+
   end
 
 end
