d28f26c5f617c5694c418f60b89ca1ffdde58f92
Darren Cauthon <darren@cauthon.com>
2014-02-07 12:13:07 -0600

    Build parser, and use the compare to build a new github URL.

--- lib/ci_in_a_can/build.rb
+++ lib/ci_in_a_can/build.rb
@@ -1,4 +1,19 @@
 module CiInACan
+
   class Build
+
+    attr_accessor :git_ssh
+
+    def self.parse content
+      data = JSON.parse content
+
+      splat = data['payload']['compare'].split('/')
+
+      build = self.new
+      build.git_ssh = "git@github.com:#{splat[3]}/#{splat[4]}.git"
+      build
+    end
+
   end
+
 end
---
+++ spec/ci_in_a_can/build_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../spec_helper'
+
+describe CiInACan::Build do
+
+  [:compare, :git_ssh].to_objects {[
+    ["https://github.com/darrencauthon/ci_in_a_can/commit/b1c5f9c9588f", "git@github.com:darrencauthon/ci_in_a_can.git"]
+  ]}.each do |test|
+
+    describe "parse" do
+
+      let(:content) do
+        {
+          payload: {
+                     compare: test.compare
+                   }
+        }.to_json
+      end
+
+      it "should convert the http to a git ssh" do
+        build = CiInACan::Build.parse content
+        build.git_ssh.must_equal test.git_ssh
+      end
+
+    end
+
+  end
+
+end
