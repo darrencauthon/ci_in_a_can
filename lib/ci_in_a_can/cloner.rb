792b29362f56284a14ea221d9d5edb5fea86d112
Darren Cauthon <darren@cauthon.com>
2014-02-07 15:57:59 -0600

    Check out the sha being tested.

--- lib/ci_in_a_can/cloner.rb
+++ lib/ci_in_a_can/cloner.rb
@@ -1,7 +1,7 @@
 module CiInACan
   module Cloner
     def self.clone_a_local_copy_for build
-      CiInACan::Bash.run "git clone #{build.git_ssh} #{build.local_location}"
+      CiInACan::Bash.run "git clone #{build.git_ssh} #{build.local_location}; cd #{build.local_location}; git checkout -b testing origin/#{build.sha}"
     end
   end
 end
--- spec/ci_in_a_can/cloner_spec.rb
+++ spec/ci_in_a_can/cloner_spec.rb
@@ -2,16 +2,16 @@ require_relative '../spec_helper'
 
 describe CiInACan::Cloner do
 
-  [:git_ssh, :local_location].to_objects { [
-    ['a', 'b'],
-    ['b', 'a'],
+  [:git_ssh, :local_location, :sha].to_objects { [
+    ['a', 'b', 'c'],
+    ['b', 'a', 'd'],
   ] }.each do |build|
 
     describe "clone a local copy for" do
 
       it "should run a bash command to clone the repo" do
 
-        CiInACan::Bash.expects(:run).with "git clone #{build.git_ssh} #{build.local_location}"
+        CiInACan::Bash.expects(:run).with "git clone #{build.git_ssh} #{build.local_location}; cd #{build.local_location}; git checkout -b testing origin/#{build.sha}"
 
         CiInACan::Cloner.clone_a_local_copy_for build
 
