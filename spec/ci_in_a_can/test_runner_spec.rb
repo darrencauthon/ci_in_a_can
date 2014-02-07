5d2d260c1db0c5242972a1ec407eeeb12c174cf1
Darren Cauthon <darren@cauthon.com>
2014-02-07 16:53:42 -0600

    Refactor the existing tests.

--- spec/ci_in_a_can/test_runner_spec.rb
+++ spec/ci_in_a_can/test_runner_spec.rb
@@ -15,29 +15,33 @@ describe CiInACan::TestRunner do
         CiInACan::Bash.stubs(:run)
       end
 
-      it "should cd into the local directory and run the default rake task" do
-        build.local_location = test.local_location
+      describe "when no special commands exist" do
 
-        CiInACan::Bash.expects(:run).with("cd #{test.local_location}; bundle exec rake")
+        it "should cd into the local directory and run the default rake task" do
+          build.local_location = test.local_location
 
-        CiInACan::TestRunner.run_tests_for build
-      end
+          CiInACan::Bash.expects(:run).with("cd #{test.local_location}; bundle exec rake")
 
-      it "should return a test result" do
-        result = CiInACan::TestRunner.run_tests_for build
-        result.is_a?(CiInACan::TestResult).must_equal true
-      end
+          CiInACan::TestRunner.run_tests_for build
+        end
 
-      it "should return a passed test result if the command returns true" do
-        CiInACan::Bash.stubs(:run).returns true
-        result = CiInACan::TestRunner.run_tests_for build
-        result.passed.must_equal true
-      end
+        it "should return a test result" do
+          result = CiInACan::TestRunner.run_tests_for build
+          result.is_a?(CiInACan::TestResult).must_equal true
+        end
 
-      it "should return a failed test result if the command returns true" do
-        CiInACan::Bash.stubs(:run).returns false
-        result = CiInACan::TestRunner.run_tests_for build
-        result.passed.must_equal false
+        it "should return a passed test result if the command returns true" do
+          CiInACan::Bash.stubs(:run).returns true
+          result = CiInACan::TestRunner.run_tests_for build
+          result.passed.must_equal true
+        end
+
+        it "should return a failed test result if the command returns true" do
+          CiInACan::Bash.stubs(:run).returns false
+          result = CiInACan::TestRunner.run_tests_for build
+          result.passed.must_equal false
+        end
+
       end
 
     end
