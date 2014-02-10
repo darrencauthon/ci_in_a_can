4d733362a095388c77c6721764bbf058af61f83d
Darren Cauthon <darren@cauthon.com>
2014-02-10 16:17:45 -0600

    Refactor.

--- lib/ci_in_a_can/build.rb
+++ lib/ci_in_a_can/build.rb
@@ -6,20 +6,36 @@ module CiInACan
     attr_accessor :id
 
     def self.parse content
-      data = JSON.parse content
-      data['payload'] = JSON.parse data['payload']
+      payload = extract_payload_from content
 
-      splat = data['payload']['compare'].split('/')
+      username, project_name = get_username_and_project_name_from payload
+      sha = extract_sha_from payload
 
       build = self.new
-      build.git_ssh = "git@github.com:#{splat[3]}/#{splat[4]}.git"
-      build.repo    = "#{splat[3]}/#{splat[4]}"
-      build.sha     = data['payload']['head_commit']['id']
+      build.git_ssh = "git@github.com:#{username}/#{project_name}.git"
+      build.repo    = "#{username}/#{project_name}"
+      build.sha     = sha
       build
     end
 
     def commands
       ['bundle install', 'bundle exec rake']
+    end
+
+    private
+
+    def self.extract_payload_from content
+      data = JSON.parse content
+      JSON.parse data['payload']
+    end
+
+    def self.get_username_and_project_name_from payload
+      splat = payload['compare'].split('/')
+      [splat[3], splat[4]]
+    end
+
+    def self.extract_sha_from payload
+      payload['head_commit']['id']
     end
 
   end
