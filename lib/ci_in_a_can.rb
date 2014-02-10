596ad32b53e3938a914c5582b5a66067f296180d
Darren Cauthon <darren@cauthon.com>
2014-02-09 23:00:53 -0600

    Use a public property to get the results location.

--- lib/ci_in_a_can.rb
+++ lib/ci_in_a_can.rb
@@ -5,8 +5,8 @@ Dir[File.dirname(__FILE__) + '/ci_in_a_can/*.rb'].each { |file| require file }
 
 module CiInACan
 
-  def self.results_location
-    File.expand_path("#{Dir.pwd}/../results")
+  class << self
+    attr_accessor :results_location
   end
 
 end
