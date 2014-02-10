3d9da262cd348b73100580b59e070c1662dc2a27
Darren Cauthon <darren@cauthon.com>
2014-02-09 20:34:11 -0600

    Test organization.

--- spec/ci_in_a_can/test_result_spec.rb
+++ spec/ci_in_a_can/test_result_spec.rb
@@ -2,17 +2,7 @@ require_relative '../spec_helper'
 
 describe CiInACan::TestRunner do
 
-  describe "create and find" do
-
-    it "should store the result" do
-
-      original_test_result  = CiInACan::TestResult.create({})
-      retrieved_test_result = CiInACan::TestResult.find original_test_result.id
-
-      original_test_result.id.must_equal retrieved_test_result.id
-
-    end
-
+  describe "create" do
     it "should set a unique id" do
       id   = Object.new
       uuid = Object.new
@@ -23,6 +13,18 @@ describe CiInACan::TestRunner do
       test_result = CiInACan::TestResult.create({})
 
       test_result.id.must_be_same_as id
+    end
+  end
+
+  describe "create and find" do
+
+    it "should be able to store and retrieve a result" do
+
+      original_test_result  = CiInACan::TestResult.create({})
+      retrieved_test_result = CiInACan::TestResult.find original_test_result.id
+
+      original_test_result.id.must_equal retrieved_test_result.id
+
     end
 
   end
