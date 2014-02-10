c93ad843c2b0f35783da85267b004168a7a13533
Darren Cauthon <darren@cauthon.com>
2014-02-09 22:02:37 -0600

    Use the new github notifing method for test results.

--- spec/ci_in_a_can/test_result_notifier_spec.rb
+++ spec/ci_in_a_can/test_result_notifier_spec.rb
@@ -7,32 +7,14 @@ describe CiInACan::TestResultNotifier do
     let(:build)       { CiInACan::Build.new }
     let(:test_result) { CiInACan::TestResult.new }
 
-    it "should mark passing tests as success in github" do
+    it "should mark the results in github" do
 
       test_result.passed = true
 
       build.sha  = Object.new
       build.repo = Object.new
 
-      client = Object.new
-      CiInACan::Github.stubs(:client).returns client
-
-      client.expects(:create_status).with(build.repo, build.sha, "success")
-
-      CiInACan::TestResultNotifier.send_for build, test_result
-    end
-
-    it "should mark failing tests as failure in github" do
-
-      test_result.passed = false
-
-      build.sha  = Object.new
-      build.repo = Object.new
-
-      client = Object.new
-      CiInACan::Github.stubs(:client).returns client
-
-      client.expects(:create_status).with(build.repo, build.sha, "failure")
+      CiInACan::Github.expects(:report_complete_status_for).with build, test_result
 
       CiInACan::TestResultNotifier.send_for build, test_result
     end
