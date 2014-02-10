752c6fd90ddabb0dd95b2e0fbf5f5ff6bc234cda
Darren Cauthon <darren@cauthon.com>
2014-02-09 21:37:06 -0600

    Do not attempt to return a github client if no access token exists.

--- lib/ci_in_a_can/github.rb
+++ lib/ci_in_a_can/github.rb
@@ -9,6 +9,7 @@ module CiInACan
       attr_accessor :access_token
 
       def client
+        return nil unless access_token
         Octokit::Client.new access_token: access_token
       end
 
--- spec/ci_in_a_can/github_spec.rb
+++ spec/ci_in_a_can/github_spec.rb
@@ -4,15 +4,34 @@ describe CiInACan::Github do
 
   describe "client" do
 
-    it "should create an Octokit client with the access token" do
-      access_token = Object.new
-      client       = Object.new
+    describe "when an access token was provided" do
 
-      CiInACan::Github.access_token = access_token
+      let(:access_token) { Object.new }
 
-      Octokit::Client.expects(:new).with(access_token: access_token).returns client
+      it "should create an Octokit client with the access token" do
+        client       = Object.new
 
-      CiInACan::Github.client.must_be_same_as client
+        CiInACan::Github.access_token = access_token
+
+        Octokit::Client.expects(:new).with(access_token: access_token).returns client
+
+        CiInACan::Github.client.must_be_same_as client
+      end
+
+    end
+
+    describe "when an access token was not provided" do
+
+      let(:access_token) { nil }
+
+      it "should return nothing" do
+        CiInACan::Github.access_token = access_token
+
+        Octokit::Client.expects(:new).never
+
+        CiInACan::Github.client.nil?.must_equal true
+      end
+
     end
 
   end
