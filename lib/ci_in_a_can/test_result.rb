151ffefb20f973992eb7bbaa421b8787e10218a1
Darren Cauthon <darren@cauthon.com>
2014-02-10 20:46:45 -0600

    Refactor.

--- lib/ci_in_a_can/test_result.rb
+++ lib/ci_in_a_can/test_result.rb
@@ -2,19 +2,32 @@ require 'subtle'
 
 module CiInACan
   class TestResult
+
+    PERSISTENCE_TYPE = 'test_result'
+
     params_constructor
+
     attr_accessor :id
     attr_accessor :passed, :output
 
     def self.create values
-      test_result = self.new values
-      test_result.id = UUID.new.generate
-      CiInACan::Persistence.save "test_result", test_result.id, test_result
+      test_result = create_the_test_result_from values
+      save_this test_result
       test_result
     end
 
     def self.find id
-      CiInACan::Persistence.find "test_result", id
+      CiInACan::Persistence.find PERSISTENCE_TYPE, id
+    end
+
+    private
+
+    def self.create_the_test_result_from values
+      self.new values.merge(id: UUID.new.generate)
+    end
+
+    def self.save_this test_result
+      CiInACan::Persistence.save PERSISTENCE_TYPE, test_result.id, test_result
     end
   end
 end
