853ac510a7178d179508518f49b33ef825d6fc8e
Darren Cauthon <darren@cauthon.com>
2014-02-10 20:53:01 -0600

    Formatting.

--- lib/ci_in_a_can/daemon.rb
+++ lib/ci_in_a_can/daemon.rb
@@ -1,10 +1,15 @@
 module CiInACan
+
   module Daemon
+
     def self.start options
       CiInACan.site_url = options[:site_url]
       CiInACan::Github.access_token = options[:access_token]
       CiInACan::Watcher.watch options[:watching_location], options[:working_location]
+
       sleep
     end
+
   end
+
 end
