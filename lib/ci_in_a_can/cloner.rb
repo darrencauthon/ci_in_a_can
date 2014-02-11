4bf2cc09f1a855a8628a68cd14dc98cfa40ae66a
Darren Cauthon <darren@cauthon.com>
2014-02-10 20:52:01 -0600

    Refactor.

--- lib/ci_in_a_can/cloner.rb
+++ lib/ci_in_a_can/cloner.rb
@@ -1,7 +1,17 @@
 module CiInACan
+
   module Cloner
+
     def self.clone_a_local_copy_for build
-      CiInACan::Bash.run "git clone #{build.git_ssh} #{build.local_location}; cd #{build.local_location}; git checkout #{build.sha}"
+      CiInACan::Bash.run commands_to_run_for(build).join('; ')
     end
+
+    def self.commands_to_run_for build
+      ["git clone #{build.git_ssh} #{build.local_location}",
+       "cd #{build.local_location}",
+       "git checkout #{build.sha}"]
+    end
+
   end
+
 end
