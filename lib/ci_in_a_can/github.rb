ce95d166a8960060bf68ddf8db76061d881b7989
Darren Cauthon <darren@cauthon.com>
2014-02-10 20:55:43 -0600

    Refactor.

--- lib/ci_in_a_can/github.rb
+++ lib/ci_in_a_can/github.rb
@@ -5,24 +5,34 @@ module CiInACan
   module Github
 
     class << self
-      
       attr_accessor :access_token
+    end
 
-      def client
-        return nil if access_token.to_s == ''
-        Octokit::Client.new access_token: access_token
-      end
+    def self.client
+      return nil if access_token.to_s == ''
+      Octokit::Client.new(access_token: access_token)
+    end
 
-      def report_pending_status_for build
-        return nil unless client
-        client.create_status build.repo, build.sha, 'pending'
-      end
+    def self.report_pending_status_for build
+      return nil unless client
+      client.create_status build.repo, build.sha, 'pending'
+    end
 
-      def report_complete_status_for build, test_result
-        return nil unless client
-        client.create_status(build.repo, build.sha, (test_result.passed ? 'success' : 'failure'), { target_url: "#{CiInACan.site_url}/test_result/#{test_result.id}" } )
-      end
+    def self.report_complete_status_for build, test_result
+      return nil unless client
+      client.create_status(build.repo, build.sha, 
+                           complete_status_from(test_result), 
+                           { target_url: target_url_for(test_result) } )
+    end
 
+    private
+
+    def self.target_url_for test_result
+      "#{CiInACan.site_url}/test_result/#{test_result.id}"
+    end
+
+    def self.complete_status_from test_result
+      (test_result.passed ? 'success' : 'failure')
     end
 
   end
