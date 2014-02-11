16362fffd554e4e930fe49d3f0493628503e015b
Darren Cauthon <darren@cauthon.com>
2014-02-10 21:05:43 -0600

    Refactor.

--- lib/ci_in_a_can/test_runner.rb
+++ lib/ci_in_a_can/test_runner.rb
@@ -1,13 +1,27 @@
 module CiInACan
+
   module TestRunner
+
     def self.run_tests_for build
-      commands = ["cd #{build.local_location}"]
-      build.commands.each { |c| commands << c }
-
-      bash_result = CiInACan::Bash.run commands.join('; ')
-
-      CiInACan::TestResult.create( { passed: bash_result.successful,
-                                     output: bash_result.output      } )
+      bash_result = run get_commands_for(build)
+      build_test_result_from bash_result
     end
+
+    private
+
+    def self.build_test_result_from bash_result
+      CiInACan::TestResult.create( { passed: bash_result.successful,
+                                     output: bash_result.output } )
+    end
+
+    def self.run commands
+      CiInACan::Bash.run commands.join('; ')
+    end
+
+    def self.get_commands_for build
+      ["cd #{build.local_location}", build.commands].flatten
+    end
+
   end
+
 end
