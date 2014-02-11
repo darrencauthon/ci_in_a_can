d82bda6802ea68df2eaf6401ca98a07204710429
Darren Cauthon <darren@cauthon.com>
2014-02-10 21:01:37 -0600

    Readability.

--- lib/ci_in_a_can/test_result_notifier.rb
+++ lib/ci_in_a_can/test_result_notifier.rb
@@ -1,7 +1,17 @@
 module CiInACan
+
   module TestResultNotifier
+
     def self.send_for build, test_result
+      report_results_to_github build, test_result
+    end
+
+    private
+
+    def self.report_results_to_github build, test_result
       CiInACan::Github.report_complete_status_for build, test_result
     end
+
   end
+
 end
