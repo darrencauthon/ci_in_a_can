92646b44f9310f4d882eac921de1c1865853692e
Darren Cauthon <darren@cauthon.com>
2014-02-14 23:54:08 -0600

    Try returning an object instead of a hash.

--- lib/ci_in_a_can/last_run_list.rb
+++ lib/ci_in_a_can/last_run_list.rb
@@ -2,6 +2,15 @@ module CiInACan
 
   module LastRunList
 
+    class TestRunResult
+      def initialize value
+        @value = value
+      end
+      def method_missing(meth, *args, &blk)
+        @value[:meth]
+      end
+    end
+
     def self.add build, test_result
       data = { 
                created_at:     test_result.created_at,
@@ -16,7 +25,7 @@ module CiInACan
 
     def self.all
       blah = CiInACan::Persistence.hash_for("test_run_list")
-      blah.sort_by { |x| x[0] }.reverse.map { |x| x[1] }
+      blah.sort_by { |x| x[0] }.reverse.map { |x| TestRunResult.new x[1] }
     end
 
   end
