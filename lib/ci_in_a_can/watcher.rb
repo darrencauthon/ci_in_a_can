b288a3f0da6acbb151a08d2c99ed683eb7f5d977
Darren Cauthon <darren@cauthon.com>
2014-02-16 19:54:05 -0600

    Refactor.

--- lib/ci_in_a_can/watcher.rb
+++ lib/ci_in_a_can/watcher.rb
@@ -23,14 +23,16 @@ module CiInACan
 
           build = create_a_build_for(new_files.first, working_location)
 
-          begin
-            File.delete new_files.first
-          rescue
-          end
+          delete new_files.first
 
           Runner.run build
 
         end
+      end
+
+      def delete file
+        File.delete file
+      rescue
       end
 
       def create_a_build_for file, working_location
