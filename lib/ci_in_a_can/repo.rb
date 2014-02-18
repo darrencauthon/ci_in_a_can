ff7793c90490efb5d3361f66dad4905f27686cc0
Darren Cauthon <darren@cauthon.com>
2014-02-17 19:44:40 -0600

    Add an api key.

--- lib/ci_in_a_can/repo.rb
+++ lib/ci_in_a_can/repo.rb
@@ -2,13 +2,16 @@ module CiInACan
 
   class Repo
 
-    params_constructor
+    params_constructor do
+      @api_key = UUID.new.generate unless @api_key
+    end
 
     attr_accessor :id
     attr_accessor :name
+    attr_accessor :api_key
 
     def self.create data
-      repo_data = { id: data[:id], name: data[:name] }
+      repo_data = { id: data[:id], name: data[:name], api_key: data[:api_key] }
       CiInACan::Persistence.save('repos', data[:id], repo_data)
     end
 
--- spec/ci_in_a_can/repo_spec.rb
+++ spec/ci_in_a_can/repo_spec.rb
@@ -4,16 +4,22 @@ describe CiInACan::Repo do
 
   before do
     clear_all_persisted_data
+
+    uuid = Object.new
+    uuid.stubs(:generate).returns api_key
+    UUID.stubs(:new).returns uuid
   end
 
   describe "create and find" do
 
-    [:id, :name].to_objects {[
-      ['abc', 'apple'],
-      ['123', 'orang']
+    [:id, :name, :api_key].to_objects {[
+      ['abc', 'apple', 'test1'],
+      ['123', 'orang', 'test2']
     ]}.each do |data|
 
       describe "finding a record by id" do
+
+        let(:api_key) { data.api_key }
 
         it "should return a repo with that id" do
 
@@ -38,11 +44,32 @@ describe CiInACan::Repo do
 
       end
 
-      it "should allow the stamping by name" do
-        CiInACan::Repo.create(id: data.id, name: data.name)
+      describe "attributes" do
 
-        repo = CiInACan::Repo.find data.id
-        repo.name.must_equal data.name
+        let(:api_key) { data.api_key }
+
+        it "should allow the stamping by name" do
+          CiInACan::Repo.create(id: data.id, name: data.name)
+
+          repo = CiInACan::Repo.find data.id
+          repo.name.must_equal data.name
+        end
+
+        it "should create a unique api key" do
+          CiInACan::Repo.create(id: data.id)
+
+          repo = CiInACan::Repo.find data.id
+          repo.api_key.must_equal data.api_key
+        end
+
+        it "should not override the existing api key" do
+          CiInACan::Repo.create(id: data.id, 
+                                api_key: 'previously set value')
+
+          repo = CiInACan::Repo.find data.id
+          repo.api_key.must_equal 'previously set value'
+        end
+
       end
 
     end
