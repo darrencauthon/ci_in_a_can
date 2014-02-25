1699331a3e228a5171a5dffae4240d9c7732945a
Darren Cauthon <darren@cauthon.com>
2014-02-24 20:48:14 -0600

    Create a file system.

---
+++ lib/ci_in_a_can/file_system.rb
@@ -0,0 +1,15 @@
+module CiInACan
+
+  module FileSystem
+
+    def self.create_file file, content
+      CiInACan::FileSystem.create_file file, content
+    end
+
+    def self.create_directory directory
+      CiInACan::FileSystem.create_directory directory
+    end
+
+  end
+
+end
