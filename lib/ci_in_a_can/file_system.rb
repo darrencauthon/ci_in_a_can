47416bec186fad37ccdcfe34732bc851617d6478
Darren Cauthon <darren@cauthon.com>
2014-02-24 21:31:08 -0600

    Fix the file system.

--- lib/ci_in_a_can/file_system.rb
+++ lib/ci_in_a_can/file_system.rb
@@ -3,11 +3,11 @@ module CiInACan
   module FileSystem
 
     def self.create_file file, content
-      CiInACan::FileSystem.create_file file, content
+      File.open(file, 'w') { |f| f.write content }
     end
 
     def self.create_directory directory
-      CiInACan::FileSystem.create_directory directory
+      Dir.mkdir(directory) unless File.exists?(directory)
     end
 
   end
