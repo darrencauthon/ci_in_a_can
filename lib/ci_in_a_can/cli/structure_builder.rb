155cf808df2807613f35355247e0aa5b6a4fe9bb
Darren Cauthon <darren@cauthon.com>
2014-02-24 21:25:52 -0600

    Refactor.

--- lib/ci_in_a_can/cli/structure_builder.rb
+++ lib/ci_in_a_can/cli/structure_builder.rb
@@ -9,18 +9,38 @@ module CiInACan
       attr_accessor :root, :id
 
       def create
-
-        create_directories ["#{@root}", "#{@root}/jobs", "#{@root}/repos", "#{@root}/web", "#{@root}/service", "#{@root}/results"]
-
-        files = ::CiInACan::Cli::Files.for @id, @root
-        create_file "#{@root}/Rakefile", files.rake_file
-        create_file "#{@root}/service/service.rb", files.service_file
-        create_file "#{@root}/web/stay_alive.rb", files.web_daemon
-        create_file "#{@root}/web/config.ru", files.web_file
-
+        create_the_directories
+        create_the_files
       end
 
       private
+
+      def create_the_directories
+        create_directories directories_to_create
+      end
+
+      def create_the_files
+        files_to_create.each { |file, content| create_file file, content }
+      end
+
+      def directories_to_create
+        ["#{@root}",
+         "#{@root}/jobs",
+         "#{@root}/repos",
+         "#{@root}/web",
+         "#{@root}/service",
+         "#{@root}/results"]
+      end
+
+      def files_to_create
+        files = ::CiInACan::Cli::Files.for @id, @root
+        {
+          "#{@root}/Rakefile"           => files.rake_file,
+          "#{@root}/service/service.rb" => files.service_file,
+          "#{@root}/web/stay_alive.rb"  => files.web_daemon,
+          "#{@root}/web/config.ru"      => files.web_file
+        }
+      end
 
       def create_directories directories
         directories.each { |d| create_directory d }
