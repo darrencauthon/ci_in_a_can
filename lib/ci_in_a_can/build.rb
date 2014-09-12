8ddb8ca251fe633304949a6b165929693e897fed
Darren Cauthon <darren@cauthon.com>
2014-09-11 22:47:13 -0500

    Refactor for readability.

--- lib/ci_in_a_can/build.rb
+++ lib/ci_in_a_can/build.rb
@@ -1,7 +1,22 @@
 module CiInACan
+
   class Build
+
     def start data
-      CiInACan::Build.flow_for(data).start data
+      workflow = create_a_workflow_considering data
+      start_the_workflow workflow, data
     end
+
+    private
+    
+    def create_a_workflow_considering data
+      CiInACan::Build.flow_for data
+    end
+
+    def start_the_workflow flow, data
+      flow.start data
+    end
+
   end
+
 end
