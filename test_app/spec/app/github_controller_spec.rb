9d54a778a5de18acf5526e071503e49da3a179bf
Darren Cauthon <darren@cauthon.com>
2014-09-11 22:17:03 -0500

    Formatting.

--- test_app/spec/app/github_controller_spec.rb
+++ test_app/spec/app/github_controller_spec.rb
@@ -1,8 +1,11 @@
 require_relative '../minitest_helper'
 
 describe GithubController do
+
   let(:controller) { GithubController.new }
+
   it "should be an application controller" do
     controller.is_a?(ApplicationController).must_equal true
   end
+
 end
