89c77f7464c3ff12e2c6aae59d38302422c73c77
Darren Cauthon <darren@cauthon.com>
2014-09-11 21:57:45 -0500

    Remove much of the code.

--- lib/ci_in_a_can.rb
+++ lib/ci_in_a_can.rb
@@ -1,17 +1,7 @@
 require 'json'
 require 'uuid'
-require 'subtle'
 
 require_relative "ci_in_a_can/version"
-require_relative "ci_in_a_can/view_models/view_model"
-Dir[File.dirname(__FILE__) + '/ci_in_a_can/*.rb'].each { |file| require file }
-Dir[File.dirname(__FILE__) + '/ci_in_a_can/view_models/*.rb'].each { |file| require file }
 
 module CiInACan
-
-  class << self
-    attr_accessor :results_location
-    attr_accessor :site_url
-  end
-
 end
--- lib/ci_in_a_can/cleaner.rb
+++
@@ -1,7 +0,0 @@
-module CiInACan
-  module Cleaner
-    def self.remove_local_copy_of build
-      CiInACan::Bash.run "rm -rf \"#{build.local_location}\""
-    end
-  end
-end
