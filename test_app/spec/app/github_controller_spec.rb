f5ab706d2d758476c2d45799f21ff0ac407c6279
Darren Cauthon <darren@cauthon.com>
2014-09-11 22:16:49 -0500

    Set up the rails app, started on the github controller.

---
+++ test_app/spec/app/github_controller_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../minitest_helper'
+
+describe GithubController do
+  let(:controller) { GithubController.new }
+  it "should be an application controller" do
+    controller.is_a?(ApplicationController).must_equal true
+  end
+end
