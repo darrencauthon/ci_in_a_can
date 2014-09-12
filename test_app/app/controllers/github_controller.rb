d76b17b52c084a6a5bfe27508654fd4af8d1528a
Darren Cauthon <darren@cauthon.com>
2014-09-12 11:09:40 -0500

    That's test code!

--- test_app/app/controllers/github_controller.rb
+++ test_app/app/controllers/github_controller.rb
@@ -1,12 +1,15 @@
 class GithubController < ApplicationController
   def push
-    #data = params
-    data = { 
-      'compare' => "https://github.com/darrencauthon/ci_in_a_can/commit/b1c5f9c9588f",
-      'ref'     => "refs/heads/blah",
-      'head_commit' => { "id" => "b1c5f9c9588fea9cb592c88905a2af349e43a4b6" }
-    }.to_json
-    data = { 'payload' => data }.to_json
+    data = params
+    
+    # test code to replicate the github request
+    #data = { 
+      #'compare' => "https://github.com/darrencauthon/ci_in_a_can/commit/b1c5f9c9588f",
+      #'ref'     => "refs/heads/blah",
+      #'head_commit' => { "id" => "b1c5f9c9588fea9cb592c88905a2af349e43a4b6" }
+    #}.to_json
+    #data = { 'payload' => data }.to_json
+    
     parser = CiInACan::GithubPayloadParser.new
     CiInACan::Build.start parser.parse(data)
     render json: {}
